8aff3811abf3510c005138748fb6cae814be5adb
Leonie Leonie <leonie@siwagroup.com>
2023-10-05 23:40:02 +0700

    add category filter and sort in filterbar

--- frontend/src/app/components/FilterBar/filter-bar.component.ts
+++ frontend/src/app/components/FilterBar/filter-bar.component.ts
@@ -13,6 +13,21 @@ export class FilterBarComponent {
     { title: 'Aurora', value: 'aurora' },
     { title: 'Nova', value: 'nova' }
   ])
+  public sortProvider = new NameValueProvider<string>([
+    { title: 'Price', value: 'price' },
+    { title: 'Stock', value: 'stock' }
+  ])
 
   public selectedBrand?: string
+  public selectedCategory?: string
+  public selectedSortBy: string = 'stock'
+  public sortAsc: boolean = false
+
+  public onSortClick() {
+    this.sortAsc = !this.sortAsc
+  }
+
+  public onSubmit() {
+    console.log(this.selectedSortBy, this.sortAsc)
+  }
 }
